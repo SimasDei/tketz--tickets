bfa7216dd08866a4ccfa7c2c166a8460b647c63d
Simas <simas@simas-dei.com>
2021-04-25 19:54:48 +0300

    use mongo env var

--- src/index.ts
+++ src/index.ts
@@ -6,21 +6,24 @@ const init = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY env variable must be set 🐼');
   }
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI env variable must be set 🐼');
+  }
 
   try {
-    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
     });
 
-    console.log('Connected to db 🦀🦀');
+    console.log('Connected to db 🐸🐸');
   } catch (error) {
     console.error(error);
   }
 
   app.listen(3000, () => {
-    console.log('listening on port 🐋 3000 🐋');
+    console.log('listening on port 🐝 3000 🐝');
   });
 };
 
